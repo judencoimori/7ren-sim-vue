5ae92e5948933a7337b1e6308441c4db8ae036bb
judencoimori <judencoimori@gmail.com>
2023-03-19 09:42:34 +0900

    change base(pubulic path)

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "./",
   server: {
     host: true,
   },
   build: {
-    outDir: "docs",
-    assetsDir: "./"
+    outDir: "docs"
   },
   resolve: {
     alias: {
